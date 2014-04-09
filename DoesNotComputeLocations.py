a06b559970486d206f9a65c735e0bddeade3ab70
MrFlash67 <MrFlash67@users.noreply.github.com>
2014-04-10 10:12:39 +1200

    Chaned 'everything' to 'locs' and hopefully added a folder.

--- DoesNotComputeLocations.py
+++ DoesNotComputeLocations.py
@@ -1,6 +1,6 @@
 from DoesNotComputeClasses import *
 
-everything = [MultipuleLocation(3, 0, 'You are in a computer. A banana runs at you', (1, ), 'Starting room', 'Requieres  5 uses of the \'go\' command', 
+locs = [MultipuleLocation(3, 0, 'You are in a computer. A banana runs at you', (1, ), 'Starting room', 'Requieres  5 uses of the \'go\' command', 
 ['You cannot move there', 'Are you sure? It looks dangorous.', 'It\'s dangorous to go alone! Don\'t go!', 'It looks just the same, and too much moving to an identical place is bad for you!', 'Fine, go.',
 'ERROR: SPPECH OVERFLOW 1', 'ERROR: SPPECH OVERFLOW 2', 'ERROR: SPPECH OVERFLOW 3'], 'Fine, go.'),
 #needs to be Blocked Location vv
@@ -13,8 +13,8 @@ Location(4, 'There is a great, big, menancing banana next to you.', (5, 6), 'Are
 
 if __name__ == '__main__':
 	print "Name is main!"
-	print everything[0].getDesc()
-	print everything[1].getDesc()
-	print everything[2].getDesc()
-	print everything[3].getDesc()
-	print everything[4].getDesc()
+	print locs[0].getDesc()
+	print locs[1].getDesc()
+	print locs[2].getDesc()
+	print locs[3].getDesc()
+	print locs[4].getDesc()
