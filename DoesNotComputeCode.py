e21c6eed8e962f32d7cbfd76fc2fd19ad223db60
MrFlash67 <MrFlash67@users.noreply.github.com>
2014-05-15 11:48:43 +1200

    Fixed? (And new 'wrong command' message)

--- DoesNotComputeCode.py
+++ DoesNotComputeCode.py
@@ -1,31 +1,43 @@
 import sys, DoesNotComputeClasses, string, DoesNotComputeFunctions
+
+def countAdd(loopNum):
+	return loopNum
+	loopNum = loopNum + 1
+
+
 def gameloop(invi, loopNum):
 	'''Main game loop'''
 	while True:
 		command = string.lower(raw_input('> '))
 		if command == 'help':
 			DoesNotComputeFunctions.showHelp()
+			countAdd(loopNum)
 		elif command == 'look around':
 			pass
+			countAdd(loopNum)
 		elif command == 'quit':
 			DoesNotComputeFunctions.exit()
+			countAdd(loopNum)
 		elif 'go' in command:
 			pass
+			countAdd(loopNum)
 		elif 'take' in command:
 			pass
+			countAdd(loopNum)
 		elif command == 'save':
 			pass
+			countAdd(loopNum)
 		elif command == 'load':
 			pass
+			countAdd(loopNum)
 		elif command in ('inv', 'i', 'inventory'):
 			print 'Your inventory contains: ' + DoesNotComputeFunctions.listStuff(invi)
+			countAdd(loopNum)
 		elif command == 'use':
 			pass
+			countAdd(loopNum)
 		else:
-			print 'Nope!'
-		return loopNum
-		loopNum = loopNum + 1
-
+			print 'Illeagal command.\nYou will be arrested posthaste.'
 if __name__ == '__main__':
 	stuffz = ['bob', 'bill']
 	gameloop(stuffz)
