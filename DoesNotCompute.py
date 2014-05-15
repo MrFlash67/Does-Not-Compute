5bc85ce0bec4c763afe23a853ccbe939af62b4ae
MrFlash67 <MrFlash67@users.noreply.github.com>
2014-05-15 13:43:23 +1200

    Added clean KeyboardInterrupt stuff and "fixed" something.

--- DoesNotCompute.py
+++ DoesNotCompute.py
@@ -1,6 +1,9 @@
-import DoesNotComputeCode as dnc, DoesNotComputeFunctions
-print
-'''Commented out for time'''
-#DoesNotComputeFunctions.intro()
-inv = ['no tea']
-loopCount = dnc.gameloop(inv, 0)
+import DoesNotComputeCode as dnc, DoesNotComputeFunctions, sys
+try:
+	print
+	'''Commented out for time'''
+	#DoesNotComputeFunctions.intro()
+	inv = ['no tea']
+	loopCount = dnc.gameloop(inv, 0)
+except KeyboardInterrupt:
+	DoesNotComputeFunctions.exit()
