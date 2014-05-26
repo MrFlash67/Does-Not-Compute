46af3c63846a53cc61c8eebf0753dcdc1cbabc04
MrFlash67 <MrFlash67@users.noreply.github.com>
2014-05-27 11:03:13 +1200

    Added some test stuff.

--- DoesNotComputeMovement.py
+++ DoesNotComputeMovement.py
@@ -2,18 +2,22 @@ import DoesNotComputeCode
 from DoesNotComputeLocations import locs
 activeLocation = 0
 def swapLocation(nowLocation, soonLocation):
-	nowLoc = nowLocation
-	nowLocGo = locs[nowLoc].whereCanGo
-	nowLocType = locs[nowloc].getLocType
+	bypass = nowLocation
+	#nowLoc = bypass
+	nowLocGo = locs[bypass].whereCanGo
+	nowLocType = locs[bypass].getLocType()
 	#^^ exists in a state of bloody stupid.
-	#return nowLoc
-	if nowLocType == 'Location' or nowLocType == 'ContainerLocation':
-		print 'Hi'
+	'''return nowLoc
+	print 'Hi a'
+	if nowLocType == 'Location' or nowLocType == 'containerLocation':
+		print 'Hi b'
+	print nowLocType
+	print 'Hi c' '''
 
 
 if __name__ == '__main__':
 	pass
-	swapLocation(1, 2)
+	swapLocation(3, 3)
 	#while True:
 	#	print activeLocation
 
