0e53274432e3c3d5d660cb34db97e022f1b2f49c
MrFlash67 <MrFlash67@users.noreply.github.com>
2014-06-12 13:36:09 +1200

    Update so I can get help.

--- DoesNotComputeCode.py
+++ DoesNotComputeCode.py
@@ -17,7 +17,18 @@ def gameloop(invi, loopNum):
 		elif command == 'quit':
 			DoesNotComputeFunctions.exit()
 		elif 'go' in command:
-			nowLoc = DoesNotComputeFunctions.goTest()
+			#nowLoc = DoesNotComputeFunctions.goTest()
+			if command == 'go n' or 'go north':
+				print 'You went north!'
+			elif command == 'go s' or 'go south':
+				print 'You went south!'
+			elif command == 'go e' or 'go east':
+				print 'You went east!'
+			elif command == 'go w' or 'go west':
+				print 'You went west!'
+			else:
+				print "Wrong!"
+
 			loopNum = countAdd(loopNum)
 		elif 'take' in command:
 			pass
