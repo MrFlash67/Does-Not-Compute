8a5734070aba83fd04cdf6cffd7f2f5590861ca3
MrFlash67 <MrFlash67@users.noreply.github.com>
2014-07-10 16:10:08 +1200

    Pretty sure it's doing what I want it to do, time to call it a day.

--- DoesNotComputeMovement.py
+++ DoesNotComputeMovement.py
@@ -1,15 +1,16 @@
 import DoesNotComputeCode
 from DoesNotComputeLocations import locs
 activeLocation = 0
-def isBorken(nowLocGo, locNum ,nowLocName):
+def isBorken(nowLocGo, locNum ,nowLocName, nowLocNum):
+	fusRoDah = nowLocGo[0]
+	print nowLocGo[0]
 	while locNum < len(nowLocGo):
-		print 'Hi! ' + str(locNum)
+		print str(locNum + 1) + ': ' + locs[fusRoDah].getName()
+		fusRoDah = fusRoDah + 1
+		#print nowLocName
+		#print nowLocGo
+		locNum = locNum + 1
 		#print locNum
-		print len(nowLocGo)
-		locNum += 1
-		print nowLocName
-		print "Hi! " + str(locNum)
-		return locNum
 	print 'You made it!'
 def swapLocation(nowLocation, soonLocation):
 	bypass = nowLocation
@@ -17,6 +18,7 @@ def swapLocation(nowLocation, soonLocation):
 	nowLocGo = locs[bypass].whereCanGo
 	nowLocType = locs[bypass].getLocType()
 	nowLocName = locs[bypass].getName()
+	nowLocNum = locs[bypass].getNumLocation()
 	#^^ exists in a state of bloody stupid.
 	"""return nowLoc
 	print 'Hi a'
@@ -26,11 +28,13 @@ def swapLocation(nowLocation, soonLocation):
 	print 'Hi c'"""
 	if nowLocType in ('containerLocation', 'Location'):
 		print "Where do you want to go?"
-		locNum = 1
+		locNum = 0
 		print 'bobbypin'
 		
-		while locNum < len(nowLocGo):
-			locNum = isBorken(nowLocGo, locNum, nowLocName)
+		#while locNum < len(nowLocGo):
+		locNum = isBorken(nowLocGo, locNum, nowLocName, nowLocNum)
+		#locNum = 2
+		#locNum = isBorken(nowLocGo, locNum, nowLocName)
 	else:
 		if nowLocType == 'MultipuleLocation':
 			locs[bypass].moveTo(soonLocation)
