fb85023eb50341e4ff95b400ce873b19d57acc5f
Bob Flash the Sixty-Seventh <MrFlash67@users.noreply.github.com>
2015-02-11 11:20:48 +1300

    Re-did the accessible location format
    Swear I did this before…

--- DoesNotComputeLocations.py
+++ DoesNotComputeLocations.py
@@ -1,15 +1,12 @@
 from DoesNotComputeClasses import *
 
 locs = [
-Location(0, 'You are in a computer. A banana runs at you', [1,2], 'Starting room', 'Requires  5 uses of the \'go\' command'),
-BlockedLocation(1, 'You are in a computer, north of where you were. There is a wall of fire just in front of you.', [0,2], "Wall Of Fire", "Has a wall of fire. Needs an item to pass. TBI.", [3,0,2], 'TFE'),
-containerLocation(['TELNET FIRE EXTINGLISHER', 'APPLE PLOT TOKEN'], 2, 'You are in a supply room. You see a TELNET FIRE EXTINGLISHER and an APPLE PLOT TOKEN. Better take both.', [1], 'Supply Room', 'Has stuff in it.'),
-Location(3, 'There is a small spike pit and a magic wand. Don\'t take the wand.', [1, 4], 'Pre-Boss room', 'Requires APT to get in, has entry to bossRoom'),
-BossLocation(4, 'You are in an arena.', [5,], 'Arena', 'Has a boss.', 'King Banana With Gun', 0),
-Location(5, "You win.", [6,7,8], 'EndA', 'Nothing'),
-Location(6, 'You win some more', (5,), "EndA", "Nothing"),
-Location(7, 'You win some more', (5,), "EndB", "Nothing"),
-Location(8, 'You win some more', (5,), "EndC", "Nothing")
+Location(0, 'You are in a computer. A banana runs at you', [1,-1,-1,2], 'Starting room', 'Requires  5 uses of the \'go\' command'),
+BlockedLocation(1, 'You are in a computer, north of where you were. There is a wall of fire just in front of you.', [3, -1, 0, 2], "Wall Of Fire", "Has a wall of fire. Needs an item to pass. TBI.", [3,0,2], 'TFE'),
+containerLocation(['TELNET FIRE EXTINGLISHER', 'APPLE PLOT TOKEN'], 2, 'You are in a supply room. You see a TELNET FIRE EXTINGLISHER and an APPLE PLOT TOKEN. Better take both.', [-1, 1, 0, -1], 'Supply Room', 'Has stuff in it.'),
+Location(3, 'There is a small spike pit and a magic wand. Don\'t take the wand.', [4,-1,-1,1], 'Pre-Boss room', 'Requires APT to get in, has entry to bossRoom'),
+BossLocation(4, 'You are in an arena.', [5,-1,-1,-1], 'Arena', 'Has a boss.', 'King Banana With Gun', 0),
+
 ]
 
 if __name__ == '__main__':
