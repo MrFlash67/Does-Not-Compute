f69e88ca009afe7187f4e608fee03455eda6d3b9
Bob Flash the Sixty-Seventh <MrFlash67@users.noreply.github.com>
2015-02-12 10:02:43 +1300

    fix pls

--- DoesNotComputeLocations.py
+++ DoesNotComputeLocations.py
@@ -2,9 +2,9 @@ from DoesNotComputeClasses import *
 
 locs = [
 Location(0, 'You are in a computer. A banana runs at you', [1,-1,-1,2], 'Starting room', 'Requires  5 uses of the \'go\' command'),
-BlockedLocation(1, 'You are in a computer, north of where you were. There is a wall of fire just in front of you.', [-1, -1, 0, 2], "Wall Of Fire", "Has a wall of fire. Needs an item to pass. TBI.", [3,0,2], 'TFE'),
+BlockedLocation(1, 'You are in a computer, north of where you were. There is a wall of fire just in front of you.', [-1, -1, 0, 2], "Wall Of Fire", "Has a wall of fire. Needs an item to pass. TBI.", [3,-1,0,2], 'TFE'),
 containerLocation(['TELNET FIRE EXTINGLISHER', 'APPLE PLOT TOKEN'], 2, 'You are in a supply room. You see a TELNET FIRE EXTINGLISHER and an APPLE PLOT TOKEN. Better take both.', [-1, 1, 0, -1], 'Supply Room', 'Has stuff in it.'),
-Location(3, 'There is a small spike pit and a magic wand. Don\'t take the wand.', [4,-1,-1,1], 'Pre-Boss room', 'Requires APT to get in, has entry to bossRoom'),
+Location(3, 'There is a small spike pit and a magic wand. Don\'t take the wand.', [4,-1,1,-1], 'Pre-Boss room', 'Requires APT to get in, has entry to bossRoom'),
 BossLocation(4, 'You are in an arena.', [5,-1,-1,-1], 'Arena', 'Has a boss.', 'King Banana With Gun', 0),
 
 ]
--- DoesNotComputeMovement.py
+++ DoesNotComputeMovement.py
@@ -5,22 +5,29 @@ def goNorth():
 	if locs[activeLocation].whereCanGo[0] == -1:
 		print 'You cannot go this way.'
 	else:
+		print str(locs[activeLocation].whereCanGo[0])
 		return locs[activeLocation].whereCanGo[0]
-def goSouth():
-	if locs[activeLocation].whereCanGo[2] == -1:
-		print 'You cannot go this way.'
-	else:
-		return locs[activeLocation].whereCanGo[2]
+
 def goEast():
 	if locs[activeLocation].whereCanGo[1] == -1:
 		print 'You cannot go this way.'
 	else:
+		print str(locs[activeLocation].whereCanGo[1])
 		return locs[activeLocation].whereCanGo[1]
+
+def goSouth():
+	if locs[activeLocation].whereCanGo[2] == -1:
+		print 'You cannot go this way.'
+	else:
+		print str(locs[activeLocation].whereCanGo[2])
+		return locs[activeLocation].whereCanGo[2]
+
 def goWest():
 	#Where the skys are blue....
 	if locs[activeLocation].whereCanGo[3] == -1:
 		print 'You cannot go this way.'
 	else:
+		print str(locs[activeLocation].whereCanGo[3])
 		return locs[activeLocation].whereCanGo[3]
 if __name__ == "__main__":
 	print "Kay"
