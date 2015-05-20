a194c52cb7dcf3e9d36111f8deaa8f48ec533f15
MrFlash67 <MrFlash67@users.noreply.github.com>
2015-05-20 15:46:59 +1200

    Aaaah.

--- DoesNotCompute.py
+++ DoesNotCompute.py
@@ -1,8 +1,8 @@
 import DoesNotComputeCode as dnc, DoesNotComputeFunctions, sys, DoesNotComputeMovement
 try:
 	print
-	'''Commented out for time'''
-	 #DoesNotComputeFunctions.intro()
+	#'''Commented out for time'''
+	DoesNotComputeFunctions.intro()
 	inv = ['NO TEA']
 	loopCount = dnc.gameloop(inv, 0)
 except KeyboardInterrupt:
