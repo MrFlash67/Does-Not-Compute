f35fbfdf79d303defc55d26c8f2060c9b20db0c8
MrFlash67 <MrFlash67@users.noreply.github.com>
2015-06-11 09:35:12 +1200

    Update DoesNotCompute.py

--- DoesNotCompute.py
+++ DoesNotCompute.py
@@ -1,5 +1,4 @@
 import DoesNotComputeCode as dnc, DoesNotComputeFunctions, sys, DoesNotComputeMovement
-print 'resize the output to take up as much of the screen as possible'
 print 'tested on 1280 x 1800'
 print
 #'''Commented out for time'''
